e9d4697a53a369c56c05d582316bf6b156aa2d15
Hunter Lannon <hunter.d.lannon@gmail.com>
2021-04-12 13:15:41 -0500

    wrote function to add tasks to projects

--- standup.py
+++ standup.py
@@ -25,41 +25,36 @@ report = {
 last_week_projects = {"unknown_project": []}
 this_week_projects = {"unknown_project": []}
 
+# Add task to project.
+def add_task(task, project, *cleanup):
+    # Remove useless fields
+    if len(task["tags"]) == 0:
+        task["tags"] = ["unknown"]
+    for tag in task["tags"]:
+        if tags_to_keep and tag not in tags_to_keep:
+            return False
+        if tag not in project:
+            project[tag] = []
+        utils.tasks.strip(task, *cleanup)
+        project[tag].append(task)
+    return True
+
+trim = ['completed', 'completed_datetime', 'tags']
+
+# check if task is in a specific time slot
+# clean it up
+# toss it in
 for task in tasks:
     if task["completed"]:
         # Implement debugging
         #print(dir(task["completed_datetime"]))
         if last_week_start < task["completed_datetime"] <  last_week_end:
-            # Remove useless fields
-            utils.tasks.strip(task, 'completed', 'completed_datetime')
-            if len(task["tags"]) == 0:
-                task["tags"] = ["unknown"]
-                #last_week_projects["unknown_project"].append(task)
-            for tag in task["tags"]:
-                if tags_to_keep and tag not in tags_to_keep:
-                    continue
-                if tag not in last_week_projects:
-                    last_week_projects[tag] = []
-                task.pop('tags', None)
-                last_week_projects[tag].append(task)
+            add_task(task, last_week_projects, *trim)
 
     elif task["due_date"] is not None:
         if this_week_start < task["due_date"] <  this_week_end:
-            # Remove useless fields
-            utils.tasks.strip(task, 'completed', 'completed_datetime')
-
-            if len(task["tags"]) == 0:
-                task["tags"] = ["unknown"]
-                #this_week_projects["unknown_project"].append(task)
-            for tag in task["tags"]:
-                if tags_to_keep and tag not in tags_to_keep:
-                    continue
-                if tag not in this_week_projects:
-                    this_week_projects[tag] = []
-                task.pop('tags', None)
-                this_week_projects[tag].append(task)
-
-    else:
+            add_task(task, this_week_projects, *trim)
+    #else:
         # Implement debugging
         #print("task not matching")
 
